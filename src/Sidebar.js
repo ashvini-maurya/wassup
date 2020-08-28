b2c836f9a12a59e0f2845d92b457fa4e0459f269
Ashvini Kumar <ashvini@esthenos.com>
2020-08-28 22:37:50 +0530

    basic setup and sidebar created

---
+++ src/Sidebar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import IconButton from "@material-ui/core/IconButton";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
