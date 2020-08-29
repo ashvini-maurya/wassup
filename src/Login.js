e4cd6a12254d117b35f5c438125ef04418b95868
Ashvini Kumar <ashvini@esthenos.com>
2020-08-29 22:25:30 +0530

    google authentication added

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import "./Login.css";
+import { auth, provider } from "./firebase";
+import { actionTypes } from "./reducer";
+import { useStateValue } from "./StateProvider";
 
 const Login = () => {
+  const [{}, dispatch] = useStateValue();
+
   const signIn = () => {
-    console.log("signedin");
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
@@ -18,7 +31,9 @@ const Login = () => {
           <h1>Sign in to Wassup</h1>
         </div>
 
-        <Button type="submit" onClick={() => signIn()}>Sign In with Google</Button>
+        <Button type="submit" onClick={() => signIn()}>
+          Sign In with Google
+        </Button>
       </div>
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -9,10 +9,11 @@ import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import SidebarChat from "./SidebarChat";
 import db from "./firebase";
 import { useParams } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 const Sidebar = () => {
   const [rooms, setRooms] = useState([]);
-  const { roomId } = useParams();
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
@@ -32,7 +33,7 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar />
+        <Avatar src={user?.photoURL} />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
